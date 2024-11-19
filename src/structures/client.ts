5843732888150897c6a7c0c1cc0347a190d6cd07
Szymon Pel <147188774+feenko@users.noreply.github.com>
2024-11-19 17:42:12 +0100

    feat: optimize module loading

--- src/structures/client.ts
+++ src/structures/client.ts
@@ -1,10 +1,10 @@
 import { Collection, Client as DiscordClient, type ClientOptions } from 'discord.js';
 
-import fs from 'fs/promises';
 import path from 'path';
 
 import { PrismaClient } from '@prisma/client';
 import { logger } from '~/lib/logger';
+import { getFiles } from '~/lib/utils';
 import type { Command } from '~/structures/command';
 import type { Event } from './event';
 
@@ -16,75 +16,42 @@ export class Client<Ready extends boolean = true> extends DiscordClient<Ready> {
     super(options);
   }
 
+  private async registerItems<T>(dir: string, extension: string, handler: (item: T) => void): Promise<void> {
+    const files = await getFiles(dir, extension);
+    await Promise.all(
+      files.map(async file => {
+        const module = await import(file);
+        const item: T = new module.default();
+        handler(item);
+      })
+    );
+  }
+
   public async registerEvents(): Promise<void> {
-    logger.info('Registering events...');
-
-    const eventsDir = path.join(__dirname, '..', 'events');
-
-    const eventFiles = await this.getFiles(eventsDir, '.ts');
-
-    for (const file of eventFiles) {
-      const eventModule = await import(file);
-      const event: Event = new eventModule.default();
-
-      if (event) {
-        if (event.once) {
-          this.once(event.name, (...args) => event.run(...args));
-        } else {
-          this.on(event.name, (...args) => event.run(...args));
-        }
-      }
-    }
-
+    await this.registerItems<Event>(path.join(__dirname, '..', 'events'), '.ts', event => {
+      const handler = (...args: Parameters<Event['run']>) => event.run(...args);
+      event.once ? this.once(event.name, handler) : this.on(event.name, handler);
+    });
     logger.info('Events have been registered!');
   }
 
   public async registerCommands(): Promise<void> {
-    logger.info('Registering commands...');
-
-    const commandsDir = path.join(__dirname, '..', 'commands');
-
-    const commandFiles = await this.getFiles(commandsDir, '.ts');
-
-    for (const file of commandFiles) {
-      const commandModule = await import(file);
-      const command: Command = new commandModule.default();
-
-      if (command) {
-        this.commands.set(command.data.name, command);
-      }
-    }
-
+    await this.registerItems<Command>(path.join(__dirname, '..', 'commands'), '.ts', command => {
+      this.commands.set(command.data.name, command);
+    });
     this.on('ready', () => {
       this.application?.commands.set(this.commands.map(command => command.data));
       logger.info('Commands have been registered!');
     });
   }
 
-  private async getFiles(dir: string, extension: string): Promise<string[]> {
-    let files: string[] = [];
-    const items = await fs.readdir(dir, { withFileTypes: true });
-
-    for (const item of items) {
-      const itemPath = path.join(dir, item.name);
-
-      if (item.isDirectory()) {
-        files = files.concat(await this.getFiles(itemPath, extension));
-      } else if (item.isFile() && item.name.endsWith(extension)) {
-        files.push(itemPath);
-      }
-    }
-
-    return files;
-  }
-
   public async init(): Promise<void> {
-    await this.registerEvents();
-    await this.registerCommands();
-
-    await this.login().catch(error => {
-      console.error(error);
+    try {
+      await Promise.all([this.registerEvents(), this.registerCommands()]);
+      await this.login();
+    } catch (error) {
+      console.log(error);
       process.exit(1);
-    });
+    }
   }
 }
