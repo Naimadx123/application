fcf6176c30f6459d846558d8aff75fc0a10a1717
Naimadx123 <damianx787@gmail.com>
2024-11-25 16:23:17 +0100

    feat: add database connection check
    Added a check for database connection before executing commands. If the
    database is not connected, an error message is returned to the user
    and command execution is halted. Also, the Prisma client has been
    enhanced with logging for error and warning levels. Translated error
    messages for database connection issues have been added in English and
    Polish locales.

--- src/structures/client.ts
+++ src/structures/client.ts
@@ -2,7 +2,7 @@ import { Collection, Client as DiscordClient, type ClientOptions } from 'discord
 
 import path from 'path';
 
-import { PrismaClient } from '@prisma/client';
+import {PrismaClient, Prisma} from '@prisma/client';
 import { logger } from '~/lib/logger';
 import { getFiles } from '~/lib/utils';
 import type { Command } from '~/structures/command';
@@ -11,10 +11,23 @@ import { Cobalt } from '~/lib/cobalt';
 import { I18n } from '~/lib/i18n';
 
 export class Client<Ready extends boolean = true> extends DiscordClient<Ready> {
-  public readonly prisma = new PrismaClient();
+  public readonly prisma = new PrismaClient(
+    {
+      log: [
+        {
+          emit: 'event',
+          level: 'error',
+        },
+        {
+          emit: 'event',
+          level: 'warn',
+        },
+      ],
+    });
   public readonly i18n = new I18n();
   public readonly cobalt = new Cobalt();
   public readonly commands = new Collection<string, Command>();
+  public dbConnected: boolean = false;
 
   public constructor(options: ClientOptions) {
     super(options);
@@ -49,8 +62,28 @@ export class Client<Ready extends boolean = true> extends DiscordClient<Ready> {
     });
   }
 
+  private async checkConnection(): Promise<void>
+  {
+    try
+    {
+      await this.prisma.$connect();
+      this.dbConnected = true;
+      await this.prisma.$disconnect();
+      logger.info('Connected to Database');
+    }
+    catch(e)
+    {
+      if(e instanceof Prisma.PrismaClientInitializationError)
+      {
+        logger.error(`Error connecting to Database (${e.errorCode}): ${e.message}`);
+        this.dbConnected = false;
+      }
+    }
+  }
+
   public async init(): Promise<void> {
     try {
+      await this.checkConnection();
       await Promise.all([this.registerEvents(), this.registerCommands(), this.i18n.init()]);
       await this.login();
     } catch (error) {
--- src/structures/command.ts
+++ src/structures/command.ts
@@ -6,10 +6,16 @@ import type {
 } from 'discord.js';
 
 export abstract class Command {
+  public isDbRequired: boolean = false;
   public readonly data: SlashCommandBuilder | SlashCommandOptionsOnlyBuilder | SlashCommandSubcommandsOnlyBuilder;
 
   public constructor(data: SlashCommandBuilder | SlashCommandOptionsOnlyBuilder | SlashCommandSubcommandsOnlyBuilder) {
     this.data = data;
+  }
+
+  public dbRequired(): Command {
+    this.isDbRequired = true;
+    return this;
   }
 
   public abstract run(interaction: ChatInputCommandInteraction, i18n: (key: string) => string): Promise<unknown>;
